1886a66ff1cfb240b0ff1e239fc69fa1e8d3d897
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 13:16:27 +0000

    changed landingPage method to be more dynamic - includes nSpaces to show number of spaces available at MakersBnB

--- src/main/java/controller/StaticPageController.java
+++ src/main/java/controller/StaticPageController.java
@@ -2,12 +2,16 @@ package com.makers.makersbnb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StaticPageController {
     @GetMapping("/")
-    public String landingPage() {
-        return "Welcome to MakersBnB";
+    public ModelAndView landingPage() {
+        ModelAndView modelAndView = new ModelAndView("/LandingPage");
+        Integer nSpaces = 100;
+        modelAndView.addObject("nSpaces", nSpaces);
+        return modelAndView;
     }
 
     @GetMapping("/contactus")
