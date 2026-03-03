0be7a7bb3ad0c480f5257fa34fd53664c5a7ae45
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 11:54:43 +0000

    added test for landing page

--- src/test/java/com/makers/makersbnb/LandingPageTest.java
+++ src/test/java/com/makers/makersbnb/LandingPageTest.java
@@ -1,4 +1,39 @@
-package com.makers.makersbnb;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LandingPageTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    public void usersAreWelcomedToTheApp() {
+        page.navigate("http://localhost:8080");
+        Locator pageBody = page.locator("body");
+        assertThat(pageBody).containsText("Welcome to MakersBnB");
+    }
 }
