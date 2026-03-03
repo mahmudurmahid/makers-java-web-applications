99aecd836e8fe9a852dd773d9e2925f822326693
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 12:04:32 +0000

    added teamPageTest without full logic - only all pre-reqs for now

---
+++ src/test/java/com/makers/makersbnb/TeamPageTest.java
@@ -0,0 +1,32 @@
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class TeamPageTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+}
