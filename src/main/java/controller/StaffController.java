622f400bc90952b4546fb0a64f56b3c3a4c75f5c
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 09:46:20 +0000

    configured StaffController to app with index page

--- src/main/java/controller/StaffController.java
+++ src/main/java/controller/StaffController.java
@@ -1,4 +1,13 @@
-package controller;
+package com.makers.makersbnb;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StaffController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Toby, Katerina, Sandy";
+    }
 }
