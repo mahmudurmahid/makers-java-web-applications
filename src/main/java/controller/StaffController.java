239515f52699150b7f7a32efffe9fa27db17da05
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 15:41:15 +0000

    changed way how team names are presented

--- src/main/java/controller/StaffController.java
+++ src/main/java/controller/StaffController.java
@@ -2,12 +2,16 @@ package com.makers.makersbnb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StaffController {
 
     @GetMapping("/team")
-    public String index() {
-        return "Toby, Katerina, Sandy";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("/Team");
+        String[] teamMembers = new String[] {"Toby, Katerina, Sandy"};
+        modelAndView.addObject("teamMembers", teamMembers);
+        return modelAndView;
     }
 }
