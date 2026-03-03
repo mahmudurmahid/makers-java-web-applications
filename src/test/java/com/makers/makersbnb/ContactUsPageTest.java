7818a8e1f418d37e9ab2538bac45c72c09681c7b
mahmudur mahid <m.mahid@icloud.com>
2026-03-03 11:57:40 +0000

    added pre reqs and all before/after methods needed for testing

--- src/test/java/com/makers/makersbnb/ContactUsPageTest.java
+++ src/test/java/com/makers/makersbnb/ContactUsPageTest.java
@@ -1,4 +1,32 @@
-package com.makers.makersbnb;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class ContactUsPageTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
 }
