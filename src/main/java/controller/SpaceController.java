231bfd039619b2db5acad2bc9924fff80dee4c6f
mahmudur mahid <m.mahid@icloud.com>
2026-03-05 14:53:05 +0000

    added POST space form method

--- src/main/java/controller/SpaceController.java
+++ src/main/java/controller/SpaceController.java
@@ -2,8 +2,10 @@ package com.makers.makersbnb;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 import com.makers.makersbnb.model.Space;
 import com.makers.makersbnb.repository.SpaceRepository;
 
@@ -28,5 +30,14 @@ public class SpaceController {
         ModelAndView newSpaceForm = new ModelAndView("spaces/new");
         newSpaceForm.addObject("space", space);
         return newSpaceForm;
+    }
+
+    @PostMapping("/spaces")
+// Spring Boot uses the form data to create an instance of space
+// which is then passed in as an arg here
+    public RedirectView create(Space space) {
+        spaceRepository.save(space);
+        // assumes you already created a method to handle `GET "/spaces"`
+        return new RedirectView("/spaces");
     }
 }
