4558fb2ae66749bdcb5b15f58d91ed4bce85d17d
mahmudur mahid <m.mahid@icloud.com>
2026-03-05 11:54:43 +0000

    added spaces ModelAndView, linked to html, linked to spaces database

--- src/main/java/controller/SpaceController.java
+++ src/main/java/controller/SpaceController.java
@@ -1,4 +1,26 @@
-package controller;
+package com.makers.makersbnb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import com.makers.makersbnb.model.Space;
+import com.makers.makersbnb.repository.SpaceRepository;
+
+import java.util.List;
+
+@RestController
 public class SpaceController {
+    @Autowired
+    SpaceRepository spaceRepository;
+
+    @GetMapping("/spaces")
+    public ModelAndView spaces() {
+        ModelAndView modelAndView = new ModelAndView("/Spaces");
+//        String[] spaces = (List<Space> spaceRepository.findAll());
+//        modelAndView.addObject("spaces", spaces);
+        modelAndView.addObject("spaces", spaceRepository.findAll());
+        return modelAndView;
+    }
+
 }
